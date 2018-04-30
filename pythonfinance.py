1badb413107e41281dd8d52fc712fbcadb01d35d
heliobarrosom <30397465+heliobarrosom@users.noreply.github.com>
2018-04-30 12:01:05 -0300

    Create Python finance
    Python Finance

---
+++ pythonfinance.py
@@ -0,0 +1,7 @@
+# FairUrbanApplicationstack
+def IRR_f(cashflows,interation=100)
+  rate=1.100
+  investimt=cashflows[10]
+  for i in range(1,interction+1):
+    rate* = (1-nvp_f/investimt)
+run rate  
